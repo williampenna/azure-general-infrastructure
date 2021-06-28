3ea715acc5e4ec5471ada93a67abb5008dc0168a
williampenna <william.oliveira@dataside.com.br>
2021-06-28 16:43:19 -0300

    Creating azure general infrastructure

---
+++ general.tf
@@ -0,0 +1,32 @@
+variable "PROJECT" {}
+variable "ENVIRONMENT" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.RESOURCE_GROUP
+  location = var.LOCATION
+}
+
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "${var.PROJECT}sa${random_string.random.result}"
+  resource_group_name      = var.RESOURCE_GROUP
+  location                 = var.LOCATION
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  depends_on = [azurerm_resource_group.rg]
+}
+
+resource "azurerm_storage_container" "sc" {
+  name                  = "${var.PROJECT}-${var.ENVIRONMENT}-sc"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+
+  depends_on = [azurerm_resource_group.rg]
+}
---
+++ vars.tf
@@ -0,0 +1,13 @@
+# SECRETS, PLEASE PROVIDE THESE VALUES IN A TFVARS FILE
+variable "SUBSCRIPTION_ID" {}
+variable "TENANT_ID" {}
+variable "AZURE_DEVOPS_PERSONAL_TOKEN" {}
+variable "AZURE_DEVOPS_ORG_SERVICE_URL" {}
+
+# GLOBAL VARIABLES
+variable "RESOURCE_GROUP" {
+  default = "will-tests"
+}
+variable "LOCATION" {
+  default = "eastus2"
+}
