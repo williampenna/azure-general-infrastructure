ebbe52ef5fc141c29a7da75472afd09848bcb340
williampenna <william.oliveira@dataside.com.br>
2021-08-10 09:55:45 -0300

    Adding event hub namespace and event hub creations

--- modules/event_hub/main.tf
+++ modules/event_hub/main.tf
@@ -14,9 +14,20 @@ resource "azurerm_eventhub_namespace" "event_hub_namespace" {
 }
 
 resource "azurerm_eventhub" "event_hub" {
-  name                = "testEventHub"
+  name                = "goodbye"
   namespace_name      = azurerm_eventhub_namespace.event_hub_namespace.name
   resource_group_name = var.RESOURCE_GROUP
   partition_count     = 1
   message_retention   = 1
 }
+
+resource "azurerm_eventhub_authorization_rule" "eh_auth_rule" {
+  name                = "testEventHubRule"
+  namespace_name      = azurerm_eventhub_namespace.event_hub_namespace.name
+  eventhub_name       = azurerm_eventhub.event_hub.name
+  resource_group_name = var.RESOURCE_GROUP
+  listen              = true
+  send                = true
+  manage              = true
+}
+
